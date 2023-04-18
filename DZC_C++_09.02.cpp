7b61129498d93c0a2a266e8677b9ca46f63d7acd
NiRenn <kigor768@gmail.com>
2023-04-18 13:16:14 +0300

    HW

---
+++ DZC_C++_09.02.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    //task1
+    setlocale(LC_ALL, "Russian");
+    cout << "\tПохорони меня, милая\n";
+    cout << "\tВ моей футболке \"Iron Maiden\"\n";
+    cout << "\tПоцелуй в последний раз\n";
+    cout << "\tИ в Аду я вспоминаю, как\n";
+    cout << "\tТы танцевала до утра\n";
+    cout << "\tВ моей футболке \"Iron Maiden\"\n";
+    cout << "\tИ образ твой – печать клейма\n";
+    cout << "\tС той стороны закрытых глаз\n";
+    cout << "\tИ я не Прощаюсь\n\n\n";
+
+
+    //task2
+
+    cout << "\tEvery\n\tHunter\n\tWants\n\tTo know\n\tWhere\n\tThe pheasant\n\tSit\n";
+
+    //task3
+
+    cout << "\tПродам специи.\n\tСоль, перец, хмели-сунели.\n";
+    cout << "\tТел.: 123-45-67\n";
+    cout << "\t\|123\t\|123\|\t123\|\t123\|\n";
+    cout << "\t\|45\t\|45 \|\t45 \|\t45 \|\n";
+    cout << "\t\|67\t\|67 \|\t67 \|\t67 \|\n";
+
+    return 0;
+}
