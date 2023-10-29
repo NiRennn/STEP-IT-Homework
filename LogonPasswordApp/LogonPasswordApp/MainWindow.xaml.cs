0af440f7c83c78dc3da9c0c38e7d0ddbe44fe6c7
NiRenn <kigor768@gmail.com>
2023-10-29 14:11:20 +0300

    HW

---
+++ LogonPasswordApp/LogonPasswordApp/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using LogonPasswordApp.Model;
+using LogonPasswordApp.View;
+using LogonPasswordApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogonPasswordApp
+{
+
+    public partial class MainWindow : Window
+    {
+        private LoginViewModel loginViewModel;
+        private RegisterViewModel registerViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            loginViewModel = new LoginViewModel();
+            registerViewModel = new RegisterViewModel();
+
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please, fill both fields.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            loginViewModel.LoginMethod(username, password);
+
+        }
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please, fill both fields.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            registerViewModel.RegisterMethod(username, password);
+
+
+        }
+        private void ForgotPassword_Click(object sender, RoutedEventArgs e)
+        {
+            Window ForgotPasswordWindow = new Window();
+            ForgotPasswordWindow.Show();
+        }
+
+    }
+}
---
+++ LogonPasswordApp/LogonPasswordApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,65 @@
+﻿using LogonPasswordApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+
+namespace LogonPasswordApp.ViewModel
+{
+    public class LoginViewModel
+    {
+        private UserDataManager userManager;
+
+        public LoginViewModel()
+        {
+            userManager = new UserDataManager();
+        }
+        public class UserDataManager
+        {
+            public const string UsersDataPath = "users.json";
+
+            public List<User> LoadUsers()
+            {
+                if (File.Exists(UsersDataPath))
+                {
+                    string json = File.ReadAllText(UsersDataPath);
+                    return JsonSerializer.Deserialize<List<User>>(json);
+                }
+                return new List<User>();
+            }
+
+            public void SaveUsers(List<User> users)
+            {
+                string json = JsonSerializer.Serialize(users);
+                File.WriteAllText(UsersDataPath, json);
+            }
+
+            public bool UserExists(string username)
+            {
+                List<User> users = LoadUsers();
+                return users.Any(user => user.Username == username);
+            }
+
+        }
+        public void LoginMethod(string username, string password)
+        {
+            List<User> users = userManager.LoadUsers();
+
+            if (users.Exists(user => user.Username == username && user.Password == password))
+            {
+                MessageBox.Show("Login successful.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("The username or password you entered is incorrect.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+    }
+}
