a5bcaafb3f12edf62f69073acf35331dfe6bb346
NiRenn <kigor768@gmail.com>
2023-11-29 13:31:20 +0300

    HW

---
+++ DZ_03.12(Computers)/DZ_03.12(Computers)/Classes/OfficeComputerBuilder.cs
@@ -0,0 +1,40 @@
+﻿using DZ_03._12_Computers_.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_03._12_Computers_.Classes
+{
+    public class OfficeComputerBuilder : IComputerBuilder
+    {
+        private Computer computer = new Computer();
+
+
+        public void BuildProcessor()
+        {
+            computer.Processor = "Intel Core i5";
+        }
+
+        public void BuildMemory()
+        {
+            computer.Memory = "8GB";
+        }
+
+        public void BuildVideoCard()
+        {
+            computer.VideoCard = "Integrated";
+        }
+
+        public void BuildHardDisk()
+        {
+            computer.HardDisk = "240GB";
+        }
+
+        public Computer GetComputer()
+        {
+            return computer;
+        }
+    }
+}
---
+++ DZ_03.12(Computers)/DZ_03.12(Computers)/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using DZ_03._12_Computers_.Classes;
+
+var officeBuilder = new OfficeComputerBuilder();
+var gamingBuilder = new GamingComputerBuilder();
+var designerBuilder = new DesignerComputerBuilder();
+
+var director = new ComputerDirector();
+
+
+
+
+director.ConstructComuter(officeBuilder);
+
+var officeComputer = officeBuilder.GetComputer();
+Console.WriteLine("Office computer setup: ");
+officeComputer.DisplayInformation();
+Console.WriteLine("---------------------------");
+
+
+director.ConstructComuter(gamingBuilder);
+
+var gamingComputer = gamingBuilder.GetComputer();
+Console.WriteLine("Gaming computer setup: ");
+officeComputer.DisplayInformation();
+Console.WriteLine("---------------------------");
+
+
+director.ConstructComuter(designerBuilder);
+
+var designerComputer = designerBuilder.GetComputer();
+Console.WriteLine("Designer computer setup: ");
+officeComputer.DisplayInformation();
