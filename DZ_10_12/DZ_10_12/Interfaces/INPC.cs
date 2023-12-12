2016d8f58855379080447e99f0c198f641305d63
NiRenn <kigor768@gmail.com>
2023-12-12 12:09:26 +0300

    HW

---
+++ DZ_10_12/DZ_10_12/Interfaces/INPC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_10_12.Interfaces
+{
+    internal interface INPC
+    {
+        void DisplayPosition(int x, int y);
+
+    }
+}
