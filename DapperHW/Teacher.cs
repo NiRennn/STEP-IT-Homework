c2e1c20325c8373b002455461ff47d98bd608901
NiRenn <kigor768@gmail.com>
2024-04-08 16:49:19 +0400

    HW

---
+++ DapperHW/Teacher.cs
@@ -0,0 +1,9 @@
+namespace DapperHW;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public decimal Salary { get; set; }
+}
